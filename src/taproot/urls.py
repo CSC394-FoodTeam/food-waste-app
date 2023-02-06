c019f81eb5725ef50342bd4924aca328708a058a
Daniel Kogeni <kogenidaniel@outlook.com>
2023-02-06 11:17:21 -0600

    Create view.py and update urls.py file

--- src/taproot/urls.py
+++ src/taproot/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout'),
+    path('callback', views.callback, name='callback'),
+
 ]

f891c548ad443e2517568d24ca6b367e995d845d
Daniel Kogeni <kogenidaniel@outlook.com>
2023-02-07 00:47:20 -0600

    Fix typo in settings.py

--- src/taproot/settings.py
+++ src/taproot/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-from os
+import os
 from pathlib import Path
 from decouple import config
 

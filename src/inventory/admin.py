44245231b37580b43612e0fd3507a501618f8fae
Dan <40920446+dan-k3@users.noreply.github.com>
2023-02-10 10:44:44 -0600

    Revert "Revert "Remove inventory app and all references""
    This reverts commit 882278f10a2173205fb7c497c840efa4783e751d.

--- src/inventory/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Inventory
-from .models import Fridge, Pantry
-
-# Register your models here.
-admin.site.register(Inventory)
-admin.site.register(Fridge)
-admin.site.register(Pantry)

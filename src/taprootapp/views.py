7aafe86d8649b26623632b08f491a45d25ce3126
Daniel Kogeni <kogenidaniel@outlook.com>
2023-02-21 16:43:09 -0600

    Put the finishing touches for demo2

--- src/taprootapp/views.py
+++ src/taprootapp/views.py
@@ -205,6 +205,10 @@ def pantryDelete(request, item_name):
 ##/FAQ/##
 def faq(request):
     return render(request, 'home/faq.html')
+
+
+def discover(request):
+    return render(request, 'home/discover.html')
 ##/Pantry/##
 #class FridgeTestCreate(CreateView):
    # model = FridgeItem

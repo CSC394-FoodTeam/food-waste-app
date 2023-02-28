9717d7207fc2faff368740f12ca74bc0afb43da6
BLAKE12385 <65238492+BLAKE12385@users.noreply.github.com>
2023-02-27 19:28:16 -0600

    Update to CRUD pages, Twitter Script

--- src/taprootapp/forms.py
+++ src/taprootapp/forms.py
@@ -1,7 +1,6 @@
 from django import forms 
 from .models import PantryItem, FridgeItem
 
-
 class PantryItemForm(forms.ModelForm):
     category = forms.ChoiceField(choices=PantryItem.PANTRY_TYPES)
 
@@ -20,11 +19,12 @@ class FridgeItemForm(forms.ModelForm):
 
     class Meta:
         model = FridgeItem
-        fields = ['item_name', 'category', 'expiry_date', 'quantity']
+        fields = ['category','item_name', 'expiry_date', 'quantity']
         widgets = {
             'item_name': forms.TextInput(attrs={'class' : 'input', 'placeholder' : 'Write something...'}),
-            'category': forms.CheckboxSelectMultiple(),
-            'expiry_date': forms.SelectDateWidget(attrs={'class':''}),
+            'category': forms.Select(),
+            'quantity': forms.NumberInput(attrs={'placeholder' : 'Enter number value...'}),
+            'expiry_date': forms.SelectDateWidget(empty_label=("Choose Year", "Choose Month", "Choose Day")),
         }
 ###
 # class FridgeTestForm(forms.ModelForm):

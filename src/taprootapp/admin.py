33f81aba3062c51d9ad1b6a06094a26388fc411c
Daniel Kogeni <kogenidaniel@outlook.com>
2023-03-06 10:58:07 -0600

    Comment out unused checkbox formfield

--- src/taprootapp/admin.py
+++ src/taprootapp/admin.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.admin import admin, UserAdmin
-from django.forms import CheckboxSelectMultiple
+# from django.forms import CheckboxSelectMultiple
 from .models import User, PantryItem, FridgeItem, Recipe
 from .forms import PantryItemForm, FridgeItemForm, RecipeForm
 
@@ -21,20 +21,20 @@ class FridgeItemAdmin(admin.ModelAdmin):
 
 class RecipeFormAdmin(admin.ModelAdmin):
     form = RecipeForm
-    formfield_overrides = {
-        models.ManyToManyField: {'widget': CheckboxSelectMultiple},
-    }
+    # formfield_overrides = {
+    #     models.ManyToManyField: {'widget': CheckboxSelectMultiple},
+    # }
 
     def save_model(self, request, obj, form, change):
         obj.user = request.user
         super().save_model(request, obj, form, change)
 
-    def formfield_for_manytomany(self, db_field, request, **kwargs):
-        formfield = super().formfield_for_manytomany(db_field, request, **kwargs)
-        if db_field.name not in ('restrictions', 'cuisine', 'flavor_profile'):
-            return formfield
-        formfield.widget = CheckboxSelectMultiple()
-        return formfield
+    # def formfield_for_manytomany(self, db_field, request, **kwargs):
+    #     formfield = super().formfield_for_manytomany(db_field, request, **kwargs)
+    #     if db_field.name not in ('restrictions', 'cuisine', 'flavor_profile'):
+    #         return formfield
+    #     formfield.widget = CheckboxSelectMultiple()
+    #     return formfield
 
 admin.site.register(User, UserAdmin)
 admin.site.register(PantryItem, PantryItemAdmin)
